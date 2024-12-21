a534289b3495c26c97cd32bc41e54d723cf62383
CHB-learner <211430209@mail.dhu.edu.cn>
2024-12-21 13:04:22 +0800

    1221

--- python222_site1/helloWorld/views.py
+++ python222_site1/helloWorld/views.py
@@ -1,3 +1,18 @@
+'''
+Author: CHB-learner 211430209@mail.dhu.edu.cn
+Date: 2024-12-21 12:53:39
+FilePath: /whynot-Django/python222_site1/helloWorld/views.py
+Description: 
+
+~请在这里写文件功能描述~
+
+
+Copyright (c) 2024 by CHB-learner 211430209@mail.dhu.edu.cn, All Rights Reserved. 
+'''
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request,'index.html')
--- python222_site1/python222_site1/settings.py
+++ python222_site1/python222_site1/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'helloWorld.apps.HelloworldConfig',  # 注册helloWorld应用
 ]
 
 MIDDLEWARE = [
