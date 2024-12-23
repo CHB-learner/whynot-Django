d1ef4b34a14e834886fc2dcc0fd30267f60830e8
CHB-learner <211430209@mail.dhu.edu.cn>
2024-12-23 17:11:29 +0800

    更新Jinja3.py文件以添加环境配置；清理index.html模板，移除不必要的注释

--- python222_site1/helloWorld/Jinja3.py
+++ python222_site1/helloWorld/Jinja3.py
@@ -12,6 +12,8 @@ Copyright (c) 2024 by CHB-learner 211430209@mail.dhu.edu.cn, All Rights Reserved
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
 from jinja2 import Environment
+
+
 def environment(**options):
     env = Environment(**options)
 
